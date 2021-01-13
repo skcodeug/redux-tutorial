f7ad51d924b1338f4cf9529cd8c8811243f3cac1
skcodeug <60166232+skcodeug@users.noreply.github.com>
2021-01-13 18:07:11 +0300

    Basic Redux store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+    default:
+      return 0;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+  console.log("Store updated!", store.getState())
+})
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+})
+
+store.dispatch({
+  type: 'SUBTRACT',
+  payload: 100,
+});
